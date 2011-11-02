a89e03a5664ebc4e7a4412db9a17f855d9dc9092
Marek Majkowski <majek04@gmail.com>
2011-11-02 15:28:34 +0000

    Always serve static data, even for debug=False

--- mud/urls.py
+++ mud/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls.defaults import patterns, include, url
-
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^static/admin/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.DJANGO_TOP_DIR + '/contrib/admin/media'}),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.STATICFILES_DIRS[0]}),
+
     url(r'^', include('mud.core.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
