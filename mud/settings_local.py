8c5a22e9bd041cd2ac07bcaf704ff9a64f5bf0bd
Marek Majkowski <majek04@gmail.com>
2011-11-02 15:28:20 +0000

    More mess in settings.

--- mud/settings_local.py
+++ mud/settings_local.py
@@ -1,18 +1,47 @@
 import os
 import json
 
-DEBUG = True
+DEBUG = False
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite',
-    }
-}
+import os, sys, urlparse
+urlparse.uses_netloc.append('postgres')
+urlparse.uses_netloc.append('mysql')
+DATABASES = {}
+try:
+    if os.environ.has_key('DATABASE_URL'):
+        url = urlparse.urlparse(os.environ['DATABASE_URL'])
+        DATABASES['default'] = {
+            'NAME':     url.path[1:],
+            'USER':     url.username,
+            'PASSWORD': url.password,
+            'HOST':     url.hostname,
+            'PORT':     url.port,
+        }
+        if url.scheme == 'postgres':
+            DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg'
+        if url.scheme == 'mysql':
+            DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+    else:
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': 'db.sqlite',
+                }
+            }
+except:
+    print "Unexpected error:", sys.exc_info()
+    raise
 
 ## Identica credentials for 'Bird' mob.
 IDENTICA_USER=''
 IDENTICA_PASS=''
 
-RABBITMQ_URL='amqp://localhost'
-SOCKJS_URL='http://localhost:3001/mud'
+if os.environ.has_key('RABBITMQ_URL'):
+    RABBITMQ_URL = os.environ['RABBITMQ_URL']
+else:
+    RABBITMQ_URL='amqp://localhost'
+
+if os.environ.has_key('RABBITMQ_URL'):
+    SOCKJS_URL='http://sockjs-mud-ws.herokuapp.com/mud'
+else:
+    SOCKJS_URL='http://localhost:3001/mud'
